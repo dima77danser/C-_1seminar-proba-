eb47f451361ec9dbd38079d694a97db82c127970
Dmitrii <lukashin1988@inbox.ru>
2023-05-24 17:14:57 +0300

    na delenie

---
+++ zadanie5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число
+// и на вычоде показывает последнюю цифру этого числа
+
+Console.WriteLine("ВВедите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num>99)&&(num<1000))
+{
+    int num2 = num%10;
+    Console.WriteLine(num2);
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число" );
+}
+
