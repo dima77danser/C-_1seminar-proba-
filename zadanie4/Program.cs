f8ea98cf96ad5233fb417bae80df6fedc8d07660
Dmitrii <lukashin1988@inbox.ru>
2023-05-24 16:49:28 +0300

    celye hisla

---
+++ zadanie4/Program.cs
@@ -0,0 +1,13 @@
+﻿// НАписать программу, которая на вход принимает одно число(N), 
+// a na vyxode pokazyvaet все целые числа в промежутке от -N do N
+
+Console.WriteLine("ВВедите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int negNumber = -1*num;
+
+while (negNumber<=num)
+{
+    Console.Write(negNumber);
+    negNumber = negNumber +1;
+}
