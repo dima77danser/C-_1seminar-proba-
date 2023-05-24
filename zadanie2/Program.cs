b9cadacf9db024ca1b9a73892dbae94a527af2bf
Dmitrii <lukashin1988@inbox.ru>
2023-05-24 16:16:28 +0300

    zadanie s sravneniem 2 hisel

---
+++ zadanie2/Program.cs
@@ -0,0 +1,21 @@
+﻿//  написать программу, 
+// которая на вход принимает 2 числа и проверяет,является 
+// ли первое число квадратом второго
+
+
+Console.WriteLine("ВВедите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ВВедите число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num ==num2*num2)
+{
+    Console.WriteLine("da");
+}
+else
+{
+    Console.WriteLine("net");
+}
+
+
