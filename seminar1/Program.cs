30d9a1967774b83bc2aa84d852a364c48a81d283
Dmitrii <lukashin1988@inbox.ru>
2023-05-24 15:39:44 +0300

    zadanie s kvadratom

---
+++ seminar1/Program.cs
@@ -0,0 +1,8 @@
+﻿// запишит программу, которая на вход принимает число и выдает его квадрат
+Console.WriteLine("ВВедите число:");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = num*num;
+
+Console.WriteLine(num2);
+ 
