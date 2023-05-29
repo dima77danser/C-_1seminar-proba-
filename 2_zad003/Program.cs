eb1b93ef1d6236278448948b84c3d9ffa51d9b51
Dmitrii <lukashin1988@inbox.ru>
2023-05-29 15:29:35 +0300

    3

---
+++ 2_zad003/Program.cs
@@ -0,0 +1,30 @@
+﻿// напишите программу,кот будет принимать
+//  на вход 2 числа и выводить, является ли
+//  2 число кратным 1
+//  Если 2 число не кратно 1 , то программа выводит 
+//  остаток от деления
+
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % num2 ==0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+
+    Console.WriteLine("ostatok ot deleniq"+num1 % num2);
+    // Канкатинацией
+    // Console.WriteLine("ostatok ot deleniq");
+    // Console.WriteLine(num1 % num2);
+    // интерполяция
+    // Console.WriteLine($"ostatok ot deleniq {num1 % num2}");
+   
+}
+
+
+
