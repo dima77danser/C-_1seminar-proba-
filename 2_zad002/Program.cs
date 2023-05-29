37410babc2c2136ae8e66f0c0747b6197244a55e
Dmitrii <lukashin1988@inbox.ru>
2023-05-29 14:52:39 +0300

    2

---
+++ 2_zad002/Program.cs
@@ -0,0 +1,15 @@
+﻿// НАписать программу, которая выводит 
+// случайное 3-значное число и удаляет
+//  2 цифру  этого числа
+
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+
+int num1 = num/100;
+int num2 = num%10;
+
+int result = num1*10+num2;
+
+Console.WriteLine(result);
+
+
