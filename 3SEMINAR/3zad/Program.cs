3a519a6f78eb747ab3199b5e4e8a1dc6b85daf24
Dmitrii <lukashin1988@inbox.ru>
2023-05-30 17:37:25 +0300

    задание с Math, rasstqnie между 2 точек

---
+++ 3SEMINAR/3zad/Program.cs
@@ -0,0 +1,17 @@
+﻿// напишите прог кот принимает на вход координаты 2 точек
+// и находит расстояние между ними в 2д пространстве
+
+Console.WriteLine("Введите число от 1 до 4");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число от 1 до 4");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число от 1 до 4");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число от 1 до 4");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2));
+Console.WriteLine(d);
