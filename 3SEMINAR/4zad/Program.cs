f4abc0d7dd9bd2c385963ffd0fef6c731f5ca383
Dmitrii <lukashin1988@inbox.ru>
2023-05-30 18:05:31 +0300

    While +Math

---
+++ 3SEMINAR/4zad/Program.cs
@@ -0,0 +1,14 @@
+﻿// НАписать прог кот на вход принимает число N
+// и выдает таблицу квадратов чисел от 1 до N
+
+
+Console.WriteLine("Введите число от 1 до 4");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while (num>=count)
+{
+    Console.WriteLine(Math.Pow(count,2));
+    count++;
+}
+
