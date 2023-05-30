5e7c3c69b2d33690ea4992de555c1923b9ee0845
Dmitrii <lukashin1988@inbox.ru>
2023-05-30 16:11:27 +0300

    координаты

---
+++ 3SEMINAR/1zad/Program.cs
@@ -0,0 +1,29 @@
+﻿// НАписать программу кот принимает на вход координаты 
+// точки (Х и У), причем х и У не равны нулю и 
+// выдает номер четверти плоскости, в кот находится эта точка
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if ((x>0) && (y>0))
+{
+    Console.WriteLine("1");
+}
+else if ((x<0) && (y>0))
+{
+    Console.WriteLine("2");
+}
+else if ((x<0) && (y<0))
+{
+    Console.WriteLine("3");
+}
+else
+//else if ((x>0) && (y<0))
+{
+    Console.WriteLine("4");
+}
+
